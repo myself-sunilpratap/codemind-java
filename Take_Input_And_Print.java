da16b0d20da4170c930e515f46aa8916180c1d63
myself-sunilpratap <98827181+myself-sunilpratap@users.noreply.github.com>
2022-11-28 15:39:56 +0530

    import java.util.Scanner; class inpandpri {     public static void main(String args[])     {         String value;         Scanner sc=new Scanner(System.in);         value=sc.nextLine();         System.out.println(value);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class inpandpri
+{
+    public static void main(String args[])
+    {
+        String value;
+        Scanner sc=new Scanner(System.in);
+        value=sc.nextLine();
+        System.out.println(value);
+    }
+}
